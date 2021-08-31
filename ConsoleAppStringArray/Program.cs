c169826b6b9ac2f60ad5f08a0c1ea76c3a5730eb
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-31 15:17:33 +0530

    Add project files.

---
+++ ConsoleAppStringArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str = new String[] { "Sunflower", "Rose", "Jasmine", "Marigold", "Lily", "Lotus" };
+            // copying an array
+            String[] Info = str;
+            foreach (string k in Info)
+            {
+                Console.WriteLine(k[2]);                
+                Console.ReadKey();            
+            }
+        }
+    }
+}
